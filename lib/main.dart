0297460e403bd922a283b2993eeba5dcd2a548ff
hommehyuk <bin6827@gmail.com>
2023-12-21 18:15:53 +0900

    Sound pool 라이브러리 준비

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,8 +23,73 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  Soundpool pool = Soundpool.fromOptions(options: SoundpoolOptions.kDefault);
+
+  List<int> _sountdIds = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initSoundPool();
+  }
+
+  Future<void> initSoundPool() async {
+    int soundId = await rootBundle
+        .load('assets/do1.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/re.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/mi.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/fa.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/sol.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/ra.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/si.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+
+    soundId = await rootBundle
+        .load('assets/do2.wav')
+        .then((soundData) => pool.load(soundData));
+
+    _sountdIds.add(soundId);
+  }
 
   @override
   Widget build(BuildContext context) {
